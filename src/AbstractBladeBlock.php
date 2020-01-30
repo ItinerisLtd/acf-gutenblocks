e0198e428262648e3cd2b34c1d74a497b99b2b60
Lukas Besch <connect@lukasbesch.com>
2020-01-30 15:29:46 +0100

    Add post_id and is_preview to template data

--- src/AbstractBladeBlock.php
+++ src/AbstractBladeBlock.php
@@ -26,7 +26,7 @@ abstract class AbstractBladeBlock extends Block implements InitializableInterfac
         return function_exists($this->getBladeEngineCallable());
     }
 
-    public function renderBlockCallback(array $block): void
+    public function renderBlockCallback(array $block, string $content = '', bool $is_preview = false, int $post_id = 0 ): void
     {
         $frontend = apply_filters(
             'acf_gutenblocks/render_block_frontend_path',
@@ -40,13 +40,13 @@ abstract class AbstractBladeBlock extends Block implements InitializableInterfac
             $block['className'] ?? '',
             $block['align'] ?? '',
         ]);
-        $template_data = array_merge(
-            [
-                'block' => $block,
-                'controller' => $this,
-            ],
-            $this->with(),
-        );
+        $template_data = [
+            'block' => $block,
+            'controller' => $this,
+            'is_preview' => $is_preview,
+            'post_id' => $post_id,
+        ];
+        $template_data = array_merge($template_data, $this->with($template_data));
 
         // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
         echo $this->getBladeEngineCallable()($frontend, $template_data);
--- src/AbstractBlock.php
+++ src/AbstractBlock.php
@@ -16,7 +16,7 @@ abstract class AbstractBlock extends Block implements InitializableInterface
         return true;
     }
 
-    public function renderBlockCallback(array $block): void
+    public function renderBlockCallback(array $block, string $content = '', bool $is_preview = false, int $post_id = 0): void
     {
         $frontend = apply_filters(
             'acf_gutenblocks/render_block_frontend_path',
@@ -41,21 +41,27 @@ abstract class AbstractBlock extends Block implements InitializableInterface
             $block['align'] ?? '',
         ]);
 
-        $controller = $this;
+        $template_data = [
+            'block' => $block,
+            'controller' => $this,
+            'is_preview' => $is_preview,
+            'post_id' => $post_id,
+        ];
+        $template_data = array_merge($template_data, $this->with($template_data));
 
-        extract($this->with());
+        extract($template_data);
 
         ob_start();
 
         // TODO: Check for remote file inclusion (WP VIP).
         // phpcs:disable WordPressVIPMinimum.Files.IncludingFile.IncludingFile
-        include apply_filters('acf_gutenblocks/render_block_html', $path, $controller);
+        include apply_filters('acf_gutenblocks/render_block_html', $path, $this);
         // phpcs:enable
 
         $html = ob_get_clean();
 
         // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-        echo apply_filters('acf_gutenblocks/render_block_html_output', $html, $controller);
+        echo apply_filters('acf_gutenblocks/render_block_html_output', $html, $this);
         // phpcs:enable
     }
 }
